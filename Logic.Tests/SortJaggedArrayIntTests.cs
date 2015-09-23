9b97ed6f20341000b294db8c01e9694a173959a4
Alexander-Ryzhkov <alexander.ryzhkov.96@gmail.com>
2015-09-23 18:23:27 +0300

    Logic and 2 unit tests added

---
+++ Logic.Tests/SortJaggedArrayIntTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Logic.Tests
+{
+    [TestClass]
+    public class SortJaggedArrayIntTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void SortDescendingByMinElement_NullReference()
+        {
+            SortJaggedArrayInt.SortDescendingByMinElement(null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void SortDescendingByMinElement_ArgumentException()
+        {
+
+            SortJaggedArrayInt.SortDescendingByMinElement(new int[][] { });
+        }
+        /*
+        [TestMethod]
+        public void SortDescendingByMinElement_OneArray()
+        {
+            int[][] arrange = {
+                                  new int[] {1},
+                                  new int[] {5, -8}
+                              };
+            int[][] act = arrange;
+            SortJaggedArrayInt.SortDescendingByMinElement(act);
+            System.Console.WriteLine(act[0][0]);
+            Assert.AreEqual(arrange, act);
+        }
+         */
+    }
+}
---
+++ Logic/SortJaggedArrayInt.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+    public static class SortJaggedArrayInt
+    {
+
+        public static void SortDescendingByMinElement(int[][] jaggedArray)
+        {
+            if (jaggedArray == null) throw new ArgumentNullException("jaggedArray");
+            if (jaggedArray.Length == 0) throw new ArgumentException("jaggedArray is empty");
+
+            for (int i = 0; i < jaggedArray.Length; i++)
+            {
+                if (jaggedArray[i] == null) throw new ArgumentNullException("jaggedArray", string.Format("inner array {0}", i));
+                if (jaggedArray[i].Length == 0) throw new ArgumentException(string.Format("inner array {0} is empty", i));
+            }
+
+
+            for (int k = 0; k < jaggedArray.Length - 1; k++)
+            {
+                for (int i = 0; i < jaggedArray.Length - 1; i++)
+                {
+                    if (jaggedArray[i].Min() < jaggedArray[i + 1].Min())
+                        Swap(jaggedArray[i], jaggedArray[i + 1]);
+
+                }
+            }
+
+        }
+
+        private static void Swap(int[] a, int[] b)
+        {
+            int[] temp = a;
+            a = b;
+            b = temp;
+        }
+
+
+
+    }
+}
